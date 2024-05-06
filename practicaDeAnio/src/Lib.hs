4104d0bbcf6cd3f6c73cf56732f16c43f133e021
santirondini <santinorondini@gmail.com>
2024-05-06 18:41:00 -0300

    Update Lib.hs

--- practicaDeAnio/src/Lib.hs
+++ practicaDeAnio/src/Lib.hs
@@ -1,3 +1,4 @@
+import GHC.Exts.Heap.Closures (GenClosure(fun))
 type Valor = Float
 type Nombre = String
 type Fundacion = Float
@@ -37,7 +38,7 @@ agregarAtraccion unaciudad nuevaatraccion = unaciudad { atracciones = nuevaatrac
 crisis:: Ciudad -> Ciudad
 crisis unaciudad = unaciudad {atracciones = init (atracciones unaciudad), costoVida=costoVida unaciudad*0.9}
 
-remodelacion :: Ciudad->Aumento->Ciudad
+remodelacion:: Ciudad->Aumento->Ciudad
 remodelacion unaciudad aumento = unaciudad {nombre = "New " ++ nombre unaciudad, costoVida=costoVida unaciudad * (1 + aumento / 100)}
 
 reevaluacion :: Ciudad -> Int -> Ciudad
@@ -54,9 +55,92 @@ transformacionSucesiva unaciudad aumento = reevaluacion (crisis (remodelacion un
 Mi forma: Por santino julian RONINI AKAK EL SANEEEEEEEEEEEEEEE
 -}
 
+type FuncionRemodelacion = Ciudad->Aumento->Ciudad 
+type Crisis = (Bool,Ciudad->Ciudad)
+type Remodelacion = (Bool,Ciudad->Aumento->Ciudad,Aumento)
+type Reevaluacion = (Bool,Ciudad->Int->Ciudad,Int)
+
+{-
+boleano = fst
+funcion = snd
+-}
+
+--Accesos:
+
+booleano = fst
+eventoC = snd
+
+aumenlet (_, _, num) = num
+booleano3 (tf, _, _) = tf
+eventoT (_,ev,_) = ev
+
+--aumenlet: aumento/cantidad de letras
+--booleano3: es como first para una tupla de 3 elementos
+
+--Cambiar el año:
+
+data Anio = UnAnio {
+    numero :: Int,
+    tieneCrisis :: Crisis,
+    tieneRemodelacion :: Remodelacion,
+    tieneReevaluacion :: Reevaluacion
+}
 
 
- 
+
+ciudadPrueba :: Ciudad
+ciudadPrueba = UnaCiudad {
+    nombre = "Kentucky",
+    fundacion = 1870,
+    atracciones = ["Parque de diversiones", "Lago para pescar", "Shopping"],
+    costoVida = 24500
+}
+
+anioPrueba :: Anio
+anioPrueba = UnAnio {
+    numero = 2022,
+    tieneCrisis = (True, crisis),
+    tieneRemodelacion = (True, remodelacion, 5.0),
+    tieneReevaluacion = (True, reevaluacion, 7)
+}
+
+
+
+hayCrisisEn:: Anio->Bool
+hayCrisisEn = booleano.tieneCrisis
+
+hayRemodelacionEn :: Anio->Bool
+hayRemodelacionEn = booleano3.tieneRemodelacion
+
+hayReevaluacionEn :: Anio->Bool
+hayReevaluacionEn = booleano3.tieneReevaluacion
+
+-- Remodelación:
+
+numeroDeAnio :: Anio->Int
+numeroDeAnio  = numero 
+
+aumentoDeAnio :: Anio->Aumento
+aumentoDeAnio anio = aumenlet (tieneRemodelacion anio) 
+
+funcionRemodelacion :: Anio->FuncionRemodelacion
+funcionRemodelacion anio = eventoT (tieneRemodelacion anio)
+
+aplicarRemoelacionDeAnioEnCiudad:: Anio->Ciudad->Ciudad
+aplicarRemoelacionDeAnioEnCiudad anio ciudad =    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
