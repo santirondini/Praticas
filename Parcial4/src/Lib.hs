76299d84d07982ddb1d9c3906dc9e0f98808dbe3
santirondini <santinorondini@gmail.com>
2024-05-13 23:09:15 -0300

    thanos

---
+++ Parcial4/src/Lib.hs
@@ -0,0 +1,106 @@
+-- Escuelita de Thanos 
+
+data Personaje = UnPersonaje {
+    edad :: Int,
+    energia :: Int,
+    habilidades :: [String],
+    nombre :: String,
+    planeta :: String
+} deriving (Eq,Show)
+
+data Guantelete = UnGuantelete {
+    material :: String,
+    gemas :: [Gema]
+}
+
+type Universo = [Personaje]
+type Gema = Personaje->Personaje
+
+--Personajes Prueba:
+
+ironman :: Personaje
+ironman = UnPersonaje 50 10 ["plata"] "tony" "tierra"
+
+hulk :: Personaje
+hulk = UnPersonaje 45 50 ["ponerse verde", "crecer 20 metros"] "bruce" "tierra"
+
+spiderman :: Personaje
+spiderman = UnPersonaje 20 25 ["sentido aracnido", "inteligencia", "tia may"] "peter" "tierra"
+
+mcu = [ironman,hulk,spiderman]
+
+dividirUniverso :: Universo->Universo
+dividirUniverso universo = drop (div (length universo) 2)  universo
+
+guanteleteCompleto :: Guantelete->Bool
+guanteleteCompleto guantelete = (length (gemas guantelete) == 6) && (material guantelete == "uru")
+
+chasquido :: Guantelete->Universo->Universo
+chasquido guantelete universo
+                            | guanteleteCompleto guantelete = dividirUniverso universo
+                            | otherwise = universo
+
+-- universo apto para pendex, si alguno de los personajes tiene menos 45
+
+aptoParaPendex :: Universo->Bool
+aptoParaPendex = any (\personaje -> edad personaje < 45)
+
+-- energia total de un universo = sumatoria de enrgias de los integrantes que tienen mas de una habilidad
+
+tienenMasDeUnaHabilidad :: Personaje->Bool
+tienenMasDeUnaHabilidad personaje = length (habilidades personaje) > 1
+
+listaDePersonajesConMasDeUnaHabilidad = filter tienenMasDeUnaHabilidad
+
+energiaTotal :: Universo->Int
+energiaTotal universo = sum (map energia (listaDePersonajesConMasDeUnaHabilidad universo))
+
+mente :: Int->Gema
+mente valor personaje = personaje {energia = energia personaje - valor}
+
+restarDiezDeEnergia :: Personaje->Personaje
+restarDiezDeEnergia p = p { energia = energia p - 10}
+
+alma :: String->Gema
+alma habilidad personaje
+                        | habilidad `elem` habilidades personaje = restarDiezDeEnergia (personaje { habilidades = filter (/= habilidad) (habilidades personaje)})
+                        | otherwise = restarDiezDeEnergia personaje
+
+tieneDosHabilidadesOMenos :: Personaje->Bool
+tieneDosHabilidadesOMenos personaje = length (habilidades personaje) <= 2
+
+poder :: Gema
+poder personaje
+               | tieneDosHabilidadesOMenos personaje = personaje {habilidades = drop 2 (habilidades personaje), energia = 0}
+               | otherwise = personaje {energia = 0}
+
+tiempo :: Gema
+tiempo personaje
+                | div (edad personaje) 2 <= 18 = personaje
+                | otherwise = personaje { edad = div (edad personaje) 2}
+
+espacio :: String->Gema
+espacio nuevoplaneta personaje = personaje { energia = energia personaje - 20, planeta = nuevoplaneta}
+
+loca :: Gema->Personaje->Personaje
+loca gema personaje = gema (gema personaje)
+
+deGoma = UnGuantelete "goma" [tiempo, alma "usar Mjolnir", loca (alma "programacion en Haskell")]
+
+utilizar:: Personaje->[Gema]->Personaje
+utilizar = foldl (\personaje gema -> gema personaje)
+
+--Sin recu:
+
+listaDePerdidasDeEnergia :: [Gemas]->Personaje->[Int]
+listaDePerdidasDeEnergia gemas victima = map 
+
+gemaMasPoderosa :: Guantelete->Personaje->Gema
+gemaMasPoderosa
+
+
+
+
+
+
+
